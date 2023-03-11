a2a435d15b62c84a14d8b8cc0dd2f9d4f898dab6
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-03-11 23:46:53 +0300

    added checkBox and actions for it

--- buttonclicker/src/main/java/com/mirea/zhumanovta/buttonclicker/MainActivity.java
+++ buttonclicker/src/main/java/com/mirea/zhumanovta/buttonclicker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnWhoAmI;
     private Button btnItIsNotMe;
 
+    private CheckBox Check;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
         textViewStudent = findViewById(R.id.tvOut);
         btnWhoAmI = findViewById(R.id.btnWhoAmI);
         btnItIsNotMe = findViewById(R.id.btnItsNotMe);
+        Check = findViewById(R.id.checkBox3);
         View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textViewStudent.setText("мой номер по списку 6");
+                Check.setChecked(!Check.isChecked());
             }
         };
         btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
@@ -32,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onMyButtonClick(View view){
         textViewStudent.setText("Это не я сделал");
+        Toast.makeText(this, "Еще один способ!", Toast.LENGTH_SHORT).show();
+        Check.setChecked(!Check.isChecked());
     }
 
 }
